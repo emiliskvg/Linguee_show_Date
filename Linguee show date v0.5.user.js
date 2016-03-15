1d6da463d2253b1488b2b15f52100347e7b07659
Emilis <emiliskvg@users.noreply.github.com>
2016-03-15 14:17:42 +0200

    Update and rename Linguee show date v0.4.user.js to Linguee show date v0.5.user.js

---
+++ Linguee show date v0.5.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Linguee show date v0.5
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  Shows the date of the translated source.
+// @author       Emilis -> inboxvartotojas@gmail.com
+// @match        www.linguee.com/*
+// @require      https://code.jquery.com/jquery-1.12.0.min.js
+// @require      https://raw.githubusercontent.com/emiliskvg/Linguee_show_Date/master/main.js
+// ==/UserScript==
